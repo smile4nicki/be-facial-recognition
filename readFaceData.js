bc5c0d3f12621fe34c0a00a14951ae342356a917
Stuart Hughes <stuarthughes@Stuarts-MacBook-Pro.local>
2018-08-20 11:24:03 +0100

    adds function to read data from images folder

---
+++ readFaceData.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const fs = require("fs");
+const fr = require("face-recognition");
+
+const dataPath = path.resolve("./data/faces");
+
+const classNames = ["stuart", "Nic"];
+
+const allFiles = fs.readdirSync(dataPath);
+const imagesByClass = classNames.map(c =>
+  allFiles
+    .filter(f => f.includes(c))
+    .map(f => path.join(dataPath, f))
+    .map(fp => fr.loadImage(fp))
+);
+
+const numTrainingFaces = 4;
+const trainDataByClass = imagesByClass.map(imgs =>
+  imgs.slice(0, numTrainingFaces)
+);
+const testDataByClass = imagesByClass.map(imgs => imgs.slice(numTrainingFaces));
+
+console.log("hi", trainDataByClass);
