a741c3c51454d9e3f035a88a6d7032b9a98bcc24
Stuart Hughes <stuarthughes@Stuarts-MacBook-Pro.local>
2018-08-22 15:03:09 +0100

    places classNames in separate JSON file to be appended if a new user is added

--- readFaceData.js
+++ readFaceData.js
@@ -3,25 +3,44 @@ const fs = require("fs");
 const fr = require("face-recognition");
 const detector = fr.FaceDetector();
 const dataPath = path.resolve("./data/faces");
-const classNames = ["Nic", "Obama", "donnie", "Rick", "stuart", "Tim"];
+let classNames;
 
-const readFaceData = async () => {
+const readFaceData = async (name = "") => {
+  if (name !== "") {
+    fs.readFile("./data/classNames.json", (err, data) => {
+      if (err) throw err;
+      const json = JSON.parse(data).classNames;
+      json.push(nameToAdd);
+      fs.writeFile(
+        "./data/classNames.json",
+        JSON.stringify(json),
+        (err, data) => {
+          if (err) throw err;
+          else console.log("File appended");
+        }
+      );
+    });
+  }
+  fs.readFile("./data/classNames.json", (err, data) => {
+    if (err) throw err;
+    classNames = JSON.parse(data).classNames;
+  });
   const allFiles = await fs.readdirSync(dataPath);
-  const imagesByClass = await classNames.map((c) =>
+  const imagesByClass = await classNames.map(c =>
     allFiles
-      .filter((f) => f.includes(c))
-      .map((f) => path.join(dataPath, f))
-      .map((fp) => {
+      .filter(f => f.includes(c))
+      .map(f => path.join(dataPath, f))
+      .map(fp => {
         const image = fr.loadImage(fp);
         return detector.detectFaces(image, 150)[0];
       })
   );
 
   const numTrainingFaces = 4;
-  const trainDataByClass = await imagesByClass.map((imgs) =>
+  const trainDataByClass = await imagesByClass.map(imgs =>
     imgs.slice(0, numTrainingFaces)
   );
-  const testDataByClass = await imagesByClass.map((imgs) =>
+  const testDataByClass = await imagesByClass.map(imgs =>
     imgs.slice(numTrainingFaces)
   );
 
